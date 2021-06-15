9bdd70ca9b90e360d0fbb924eca7b42372ef5906
otonvd <otonvazquezdoce@gmail.com>
2021-06-15 12:27:23 +0200

    Create run.C

---
+++ run.C
@@ -0,0 +1,26 @@
+//#include "SDD.h"
+
+void run(char *filein ="20210610_1956_0611_0656.root",char *filecalib="foutSDD_20210517_1236_0517_1856_20kV_100uA_xray_beams.root")
+{ 
+//gSystem->AddIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_PHYSICS/include");
+
+
+//gSystem->Load("SDD_C.so");
+R__LOAD_LIBRARY(SDD_C);
+
+//gSystem->AddLinkedLibs("-L/home/oton/ALICE/sw/ubuntu2004_x86-64/ROOT/v6-20-08-alice1-1/lib -l*");
+
+//SDD* ttt = new SDD();
+ SDD* ttt = new SDD(filein,filecalib);
+
+//ttt->SetCalib(filecalib);
+
+//  ttt->Loop();
+ // Int_t InjectionFlag = 2; // ANALYZE ALL EVENTS
+ Int_t InjectionFlag = 0; // reject injection
+ //Int_t InjectionFlag = 1; // analyze only injection events
+ ttt->Loop(InjectionFlag);
+
+
+}
+
